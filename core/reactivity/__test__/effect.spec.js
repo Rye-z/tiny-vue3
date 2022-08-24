d1b996f33361c591035e6f6d089cedf5e6e09f64
rye-z <894544438@qq.com>
2022-08-24 14:03:48 +0800

    分支切换 / 遗留副作用函数清除

--- core/reactivity/__test__/effect.spec.js
+++ core/reactivity/__test__/effect.spec.js
@@ -11,4 +11,25 @@ describe('effect', function() {
     counter.num = 7
     expect(dummy).toBe(8)
   });
+  it('分支切换 / 清除遗留富副作用函数', function() {
+    let text
+    const obj = reactive({ ok: true, text: 'hello'})
+
+    const fn = jest.fn(() => {
+      text = obj.ok ? obj.text : "sorry"
+    })
+    effect(fn)
+    expect(fn).toHaveBeenCalledTimes(1)
+
+    expect(text).toBe('hello')
+    obj.ok = false
+    expect(fn).toHaveBeenCalledTimes(2)
+    expect(text).toBe('sorry')
+    /**
+     * 1.分支切换：因为 obj.ok 为 false，所以 obj.text 永远不会执行
+     *   所以当 obj.text 改变时，副作用函数不应该被触发
+     */
+    obj.text = 'change'
+    expect(fn).toHaveBeenCalledTimes(2)
+  });
 });
--- core/reactivity/effect.js
+++ core/reactivity/effect.js
@@ -1,9 +1,26 @@
 let activeEffect = null
 const bucket = new WeakMap()
 
+function cleanUpEffects(effect) {
+  effect.deps.forEach(deps => {
+    // target
+    //  - deps
+    //    - effect
+    // effect 存储的是 deps -> 先遍历删除当前的 effect
+    deps.delete(effect)
+  })
+  // 清空当前 effect 的 deps
+  effect.deps.length = 0
+}
+
 export function effect(fn) {
-  activeEffect = fn
-  fn()
+  const effectFn = () => {
+    cleanUpEffects(effectFn)
+    activeEffect = effectFn
+    fn()
+  }
+  effectFn.deps = []
+  effectFn()
 }
 
 export function track(target, key) {
@@ -16,6 +33,7 @@ export function track(target, key) {
     depsMap.set(key, deps=new Set())
   }
   deps.add(activeEffect)
+  activeEffect.deps.push(deps)
 }
 
 export function trigger(target, key) {
@@ -27,5 +45,14 @@ export function trigger(target, key) {
   if (!deps) {
     return
   }
-  deps.forEach(effect => effect())
+  // 当执行 effect 时，先 cleanUp 遗留的副作用函数，但是执行 effectFn，又会触发属性访问，在遍历的时候又会将 effect 添加到 deps 中
+  // 相当于
+  // set = new Set([1])
+  // set.forEach(item => {
+  //   set.delete(1)
+  //   set.add(1)
+  // })
+  // 使用一个新的 Set 来进行遍历，防止无限循环
+  const depsToRun = new Set(deps)
+  depsToRun.forEach(effect => effect())
 }
