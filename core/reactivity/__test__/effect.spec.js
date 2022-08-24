b1b97bdcf7f7a29869ef15b13aac891fbfaeb3ee
rye-z <894544438@qq.com>
2022-08-25 00:14:40 +0800

    4.7调度执行 连续多次修改响应式数据，只触发一次更新

--- core/reactivity/__test__/effect.spec.js
+++ core/reactivity/__test__/effect.spec.js
@@ -88,4 +88,48 @@ describe('scheduler', () => {
     jest.runAllTimers()
     expect(bar).toBe(0)
   });
+
+  it('连续多次修改响应式数据，只触发一次更新', async function() {
+    // 创建一个 promise 实例，利用它将一个微任务添加到微任务队列
+    const p = Promise.resolve()
+    const jobQueue = new Set()
+    // 是否正在刷新队列
+    let isFlushing = false
+    function flushJob() {
+      if (isFlushing) {
+        return
+      }
+      isFlushing = true
+      // 在微任务队列中刷新 jobQueue()
+      p.then(() => {
+        jobQueue.forEach(job => job())
+      }).finally(() => {
+        isFlushing = false
+      })
+    }
+
+    const obj = reactive({ foo: 1})
+
+    const fn = jest.fn(() => console.log(obj.foo))
+
+    effect(fn, {
+      scheduler(fn) {
+        // 每次调度时将 fn 放到微任务队列
+        jobQueue.add(fn)
+        flushJob()
+      }
+    })
+    obj.foo++
+    obj.foo++
+    obj.foo++
+    obj.foo++
+
+    await Promise.resolve()
+    /* 这是一个模拟：
+       - 在 Vue 中实现了一个很完善的调度器
+       - 连续多次修改响应式数据，但是只会触发一次更新
+    * */
+    expect(fn).toHaveBeenCalledTimes(2)
+    expect(obj.foo).toBe(5)
+  });
 });
