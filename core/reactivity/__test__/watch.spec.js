1b2b2c812b6c0768e6d9aba85ec469ec90d9b15f
rye-z <894544438@qq.com>
2022-08-25 10:34:02 +0800

    4.8 watch的实现原理 对象任意属性都可以触发回调

--- core/reactivity/__test__/watch.spec.js
+++ core/reactivity/__test__/watch.spec.js
@@ -2,13 +2,29 @@ import { watch } from '../watch';
 import { reactive } from '../reactive';
 
 describe('watch', function() {
-  it('监听响应式对象并触发回调', function() {
-    const obj = reactive({foo: 1})
+  it('监听响应式对象foo属性 并触发回调', function() {
+    const obj = reactive({ foo: 1 })
     const cb = jest.fn(() => obj.foo)
     watch(obj, cb)
 
     expect(cb).toHaveBeenCalledTimes(0)
     obj.foo++
     expect(cb).toHaveBeenCalledTimes(1)
+  });
+
+  it('对象任意属性都可以触发回调', function() {
+    const obj = reactive({ foo: 1, bar: 1 })
+    const cb = jest.fn(() => 1)
+    watch(obj, cb)
+
+    expect(cb).toHaveBeenCalledTimes(0)
+    obj.foo++
+    expect(cb).toHaveBeenCalledTimes(1)
+    obj.bar++
+    expect(cb).toHaveBeenCalledTimes(2)
+  });
+
+  // todo
+  it('嵌套属性触发回调', function() {
   });
 });
--- core/reactivity/watch.js
+++ core/reactivity/watch.js
@@ -1,7 +1,24 @@
 import { effect } from './effect';
 
+
+function traverse(value, seen) {
+  if (typeof value !== 'object' || value === null || seen.has(value)) {
+    return
+  }
+  // 添加到 seen 中，防止循环引用造成的死循环
+  seen.add(value)
+  // 暂不考虑其他数据结构，仅考虑 Object
+  for (const key in value) {
+    traverse(value[key], seen)
+  }
+
+  return value
+}
+
 export function watch(source, cb) {
-  effect(() => source.foo, {
+  const seen = new Set()
+
+  effect(() => traverse(source, seen), {
     scheduler() {
       cb()
     }
