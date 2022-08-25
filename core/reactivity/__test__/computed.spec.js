f3a9b294b37d039fd526243dfb209b8ba0fa739a
rye-z <894544438@qq.com>
2022-08-25 09:48:51 +0800

    4.8computed和lazy 计算缓存实现

--- core/reactivity/__test__/computed.spec.js
+++ core/reactivity/__test__/computed.spec.js
@@ -1,7 +1,15 @@
 import { reactive } from '../reactive';
 import { computed } from '../computed';
+import { effect } from '../effect';
 
 describe('computed', function() {
+  it('可以通过 value 访问', function() {
+    const obj = reactive({ foo: 1})
+    const getter = jest.fn(() => obj.foo)
+    const bar = computed(getter)
+    expect(bar.value).toBe(1)
+  });
+
   it('延迟访问', function() {
     const obj = reactive({ foo: 1})
     const getter = jest.fn(() => obj.foo)
@@ -12,8 +20,26 @@ describe('computed', function() {
     expect(bar.value).toBe(1)
     expect(getter).toHaveBeenCalledTimes(1)
     // [[set]] 触发副作用函数执行
-    obj.foo++
-    expect(getter).toHaveBeenCalledTimes(2)
+    obj.foo++ // 此时运行 scheduler，`dirty=true`，但 getter 还没执行
+    expect(getter).toHaveBeenCalledTimes(1)
+    // 访问 value 属性，getter 执行
     expect(bar.value).toBe(2)
+  });
+
+  it('计算缓存', function() {
+    // 只有响应式数据改变后，访问 computed 会重新计算
+    // [[get]] 操作不会触发副作用函数
+    const obj = reactive({ foo: 1})
+    const getter = jest.fn(() => obj.foo)
+    const bar = computed(getter)
+    // 因为延迟访问，所以此时 getter 尚未执行
+    expect(getter).toHaveBeenCalledTimes(0)
+    bar.value
+    expect(getter).toHaveBeenCalledTimes(1)
+    bar.value // 仅仅访问值，不触发重新计算
+    expect(getter).toHaveBeenCalledTimes(1)
+    obj.foo++ // 修改值
+    bar.value // 访问 value 触发重新计算
+    expect(getter).toHaveBeenCalledTimes(2)
   });
 });
--- core/reactivity/computed.js
+++ core/reactivity/computed.js
@@ -1,13 +1,26 @@
 import { effect } from './effect';
 
 export function computed(getter) {
+  let dirty = true
+  let cache
+
   const runner = effect(getter, {
-    lazy: true
+    lazy: true,
+    scheduler() {
+      // 在 trigger 也就是响应式数据发生改变的时候，将 dirty 设为 true
+      // 这里 scheduler 中不需要执行 effectFn，因为已经 effect 已经返回了 runner
+      // 可以在 get value() 时手动执行
+      dirty = true
+    }
   })
 
   return {
     get value() {
-      return runner()
+      if (dirty) {
+        cache = runner()
+        dirty = false
+      }
+      return cache
     }
   }
 }
