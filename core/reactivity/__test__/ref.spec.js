475204380681f56f2fa6681dacdee2ecbb8e2cbd
rye-z <894544438@qq.com>
2022-08-27 20:45:37 +0800

    6.2 toRef / toRefs

--- core/reactivity/__test__/ref.spec.js
+++ core/reactivity/__test__/ref.spec.js
@@ -1,6 +1,8 @@
 import {
   isRef,
-  ref
+  ref,
+  toRef,
+  toRefs
 } from '../ref';
 import { effect } from '../effect';
 import { reactive } from '../reactive';
@@ -22,10 +24,37 @@ describe('ref', function() {
 
   it('isRef', function() {
     const a = ref('a')
-    const b = reactive({b: 'b'})
+    const b = reactive({ b: 'b' })
     const c = 'c'
     expect(isRef(a)).toBe(true)
     expect(isRef(b)).toBe(false)
     expect(isRef(c)).toBe(false)
+  });
+
+  it('toRef', function() {
+    // toRef 可以解决展开运算符会导致响应性丢失的问题
+    const obj = reactive({ a: 'a', b: 'b' })
+    const newObj = {
+      a: toRef(obj, 'a'),
+      b: toRef(obj, 'b')
+    }
+
+    const fn = jest.fn(() => newObj.a.value)
+    effect(fn)
+    expect(fn).toHaveBeenCalledTimes(1)
+    newObj.a.value++
+    expect(fn).toHaveBeenCalledTimes(2)
+  });
+
+  it('toRefs', function() {
+    const obj = reactive({ a: 'a', b: 'b' })
+    const newObj = toRefs(obj)
+
+    const fn = jest.fn(() => newObj.a.value)
+    effect(fn)
+    expect(fn).toHaveBeenCalledTimes(1)
+    newObj.a.value++
+    expect(fn).toHaveBeenCalledTimes(2)
+
   });
 });
--- core/reactivity/ref.js
+++ core/reactivity/ref.js
@@ -4,13 +4,40 @@ export function isRef(value) {
   return !!value._v_isRef
 }
 
+export function toRefs(obj) {
+  const refs = {}
+  for (const k in obj) {
+    refs[k] = toRef(obj, k)
+  }
+  return refs
+}
+
+export function toRef(obj, key) {
+  // 将 reactive 对象的某个值，转为 ref
+  const wrapper = {
+    get value() {
+      return obj[key]
+    },
+    set value(newVal) {
+      obj[key] = newVal
+    }
+  }
+
+  Object.defineProperty(wrapper, '_v_isRef', {
+    // 默认 enumerable: false
+    value: true
+  })
+
+  return wrapper
+}
+
 export function ref(value) {
   const wrapper = {
     get value() {
       return value
     },
-    set value(val) {
-      value = val
+    set value(newVal) {
+      value = newVal
     }
   }
   Object.defineProperty(wrapper, '_v_isRef', {
